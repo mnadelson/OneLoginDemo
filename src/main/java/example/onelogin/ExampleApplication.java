3611bdecd5702e6d0ad83c1e970a535cfcc95909
mnadelson <mnadelson@gmail.com>
2021-02-04 09:30:17 -0500

    initial commit

---
+++ src/main/java/example/onelogin/ExampleApplication.java
@@ -0,0 +1,48 @@
+package example.onelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
+import java.security.Principal;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+@RestController
+public class ExampleApplication extends WebSecurityConfigurerAdapter {
+
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+
+    @GetMapping("/echo/{value}")
+    public String echo(@PathVariable("value") String value) {
+        return value.toUpperCase();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**", "/error**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().logout().logoutSuccessUrl("/").permitAll()
+                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
+
+    public static void main(String args[]) {
+        SpringApplication.run(ExampleApplication.class, args);
+    }
+}
